7f90fb1273a05c50087c94356d0cbd7817f53e9e
hlee <horace_lee@163.com>
2009-07-01 16:04:09 +0800

    modify one mistake

--- app/models/lib_mailer.rb
+++ app/models/lib_mailer.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -18,7 +18,7 @@
 class LibMailer < Mailer
 
   def lib_update(hch, last_id)
-  #µ±³ÖÓĞÈË¸Ä±äÊ± ·¢ËÍÓÊ¼ş
+  #å½“æŒæœ‰äººæ”¹å˜æ—¶ å‘é€é‚®ä»¶
     if hch.treasure_type=='Book'
 		name=Book.find(hch.treasure_id).title
 	elsif hch.treasure_type=='Device'
@@ -28,20 +28,20 @@ class LibMailer < Mailer
 	end
     redmine_headers 'Type' => hch.treasure_type,
                     'Name' => name
-    
+
     mail_addresses = [ User.find(last_id).mail ]
     mail_addresses << User.find(hch.updater_id).mail
 	mail_addresses << User.find(hch.holder_id).mail
-    
+
     recipients mail_addresses.compact.uniq
 	sent_on Time.now
 =begin
 #{l(:label_faq_new)}
 =end
     if hch.treasure_type=='Book'
-	  subject "#{l(:text_update_subject1)}"
+	  subject "#{l(:text_update_subject1,:name=>name)}"
 	elsif hch.treasure_type=='Device'
-	  subject "#{l(:text_update_subject2)}"
+	  subject "#{l(:text_update_subject2,:name=>name)}"
 	end
 #    subject "#{hch.treasure_type} - #{name} #{l(:text_holder_change1)} #{User.find(last_id).name} #{l(:text_holder_change2)} #{User.find(hch.holder_id).name} #{l(:text_holder_change3)} #{User.find(hch.updater_id).name}"
     body   :hch => hch,
@@ -50,10 +50,10 @@ class LibMailer < Mailer
 #@body(:recipient => recipient)
 
     content_type "multipart/alternative"
-   part :content_type => "text/plain",  
-        :body => render_message("lib_update.text.plain.rhtml", :hch => hch)  
-   part :content_type => "text/html",  
-        :body => render_message("lib_update.text.html.rhtml", :hch => hch)  	
+   part :content_type => "text/plain",
+        :body => render_message("lib_update.text.plain.rhtml", :hch => hch)
+   part :content_type => "text/html",
+        :body => render_message("lib_update.text.html.rhtml", :hch => hch)
 =begin
     part "text/plain" do |p|
       p.body = render_message("lib_update.text.plain.rhtml", :hch => hch)
@@ -64,7 +64,7 @@ class LibMailer < Mailer
     end
 =end
   end
-#ĞÂ´´½¨Ê±Ö±½ÓÖ¸¶¨³ÖÓĞÈËÊ±µÄÓÊ¼ş
+#æ–°åˆ›å»ºæ—¶ç›´æ¥æŒ‡å®šæŒæœ‰äººæ—¶çš„é‚®ä»¶
   def lib_new(hch)
 
     if hch.treasure_type=='Book'
@@ -76,29 +76,29 @@ class LibMailer < Mailer
 	end
     redmine_headers 'Type' => hch.treasure_type,
                     'Name' => name
-    
+
     mail_addresses = [ User.find(hch.holder_id).mail ]
     mail_addresses << User.find(hch.updater_id).mail
-    
+
     recipients mail_addresses.compact.uniq
 	sent_on Time.now
 
 #    subject "#{hch.treasure_type} - #{name} #{l(:text_new_mail2)} #{User.find(hch.holder_id).name} #{l(:text_holder_change3)} #{User.find(hch.updater_id).name}"
     if hch.treasure_type=='Book'
-	  subject "#{l(:text_new_subject1)}"
+	  subject "#{l(:text_new_subject1,:name=>name)}"
 	elsif hch.treasure_type=='Device'
-	  subject "#{l(:text_new_subject2)}"
+	  subject "#{l(:text_new_subject2,:name=>name)}"
 	end
     body   :hch => hch,
 		   :name => name
 
     content_type "multipart/alternative"
-   part :content_type => "text/plain",  
-        :body => render_message("lib_new.text.plain.rhtml", :hch => hch)  
-   part :content_type => "text/html",  
-        :body => render_message("lib_new.text.html.rhtml", :hch => hch)  	
-  end  
-  #·¢ËÍ¶ÔÕËµ¥¹¦ÄÜ£ºÏòËùÓĞÉè±¸»òÍ¼ÊéµÄ³ÖÓĞÈË·¢ËÍÒ»·â¶ÔÕËµ¥ÓÊ¼ş£¬ÓÊ¼şÄÚÈİÊÇ¸ÃÓÃ»§³ÖÓĞµÄÍ¼ÊéºÍÉè±¸µÄÁĞ±í£¨º¬Éè±¸Ãû³Æ¡¢±àºÅ¡¢·ÃÎÊurl¡¢ÈëÊÖÊ±¼äµÈ£©
+   part :content_type => "text/plain",
+        :body => render_message("lib_new.text.plain.rhtml", :hch => hch)
+   part :content_type => "text/html",
+        :body => render_message("lib_new.text.html.rhtml", :hch => hch)
+  end
+  #å‘é€å¯¹è´¦å•åŠŸèƒ½ï¼šå‘æ‰€æœ‰è®¾å¤‡æˆ–å›¾ä¹¦çš„æŒæœ‰äººå‘é€ä¸€å°å¯¹è´¦å•é‚®ä»¶ï¼Œé‚®ä»¶å†…å®¹æ˜¯è¯¥ç”¨æˆ·æŒæœ‰çš„å›¾ä¹¦å’Œè®¾å¤‡çš„åˆ—è¡¨ï¼ˆå«è®¾å¤‡åç§°ã€ç¼–å·ã€è®¿é—®urlã€å…¥æ‰‹æ—¶é—´ç­‰ï¼‰
   def send_statement
     bk_list=Book.find_by_sql("SELECT distinct holder_id FROM books ")
     de_list=Device.find_by_sql("SELECT distinct holder_id FROM devices")
@@ -111,21 +111,21 @@ class LibMailer < Mailer
     send_statement_each(l)
     }
   end
-  
+
   def send_statement_each(id)
     user=User.find(id)
     redmine_headers 'Type' => "notice",
                     'Name' => user.name
-    
+
     mail_addresses = [ user.mail ]
     recipients mail_addresses.compact.uniq
     sent_on Time.now
     subject "#{l(:text_send_all)}"
         content_type "multipart/alternative"
-   part :content_type => "text/plain",  
-        :body => render_message("send_statement.text.plain.rhtml", :id => id)  
-   part :content_type => "text/html",  
-        :body => render_message("send_statement.text.html.rhtml", :id => id) 
-	
+   part :content_type => "text/plain",
+        :body => render_message("send_statement.text.plain.rhtml", :id => id)
+   part :content_type => "text/html",
+        :body => render_message("send_statement.text.html.rhtml", :id => id)
+
   end
 end
